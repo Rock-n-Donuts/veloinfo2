09350bf8fb35ac949cd9f0870aa6c78689521e51
Martin Hamel <martin@ma4s.org>
2024-03-14 17:53:37 +0000

    create materialized view

--- import.sh
+++ import.sh
@@ -12,4 +12,23 @@ psql -h db -U postgres -d carte -c "CREATE OR REPLACE VIEW bike_path AS
                                                 FROM cyclability_score cs 
                                                 JOIN cycleway_way c ON c.way_id = ANY(cs.way_ids)
                                             ) t
-                                        WHERE t.rn = 1;"
+                                        WHERE t.rn = 1;
+                                        
+                                        drop materialized view edge;
+                                        drop sequence edge_id;
+                                        CREATE SEQUENCE edge_id;
+                                        CREATE MATERIALIZED VIEW edge 
+                                        AS SELECT  
+                                            nextval('edge_id')  as id, 
+                                            node as source,
+                                            lead(node) over (partition by way_id order by seq) as target,
+                                            st_x(st_transform(ST_PointN(geom, seq), 4326)) as x1,
+                                            st_y(st_transform(ST_PointN(geom, seq), 4326)) as y1,
+                                            st_x(st_transform(ST_PointN(geom, seq+1), 4326)) as x2,
+                                            st_y(st_transform(ST_PointN(geom, seq+1), 4326)) as y2,
+                                            way_id
+                                        from (
+                                            select way_id, unnest(nodes) as node, generate_series(1, array_length(nodes, 1)) as seq, geom
+                                            from cycleway_way
+                                        ) as edges;                           
+                                        "
