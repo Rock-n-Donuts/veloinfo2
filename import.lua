073fd53fdbe2efc1968d5e4da893ed974e006bce
Martin Hamel <martin@ma4s.org>
2024-04-06 14:26:59 +0000

    landeuse

--- import.lua
+++ import.lua
@@ -1,72 +1,100 @@
-local cycleway = osm2pgsql.define_way_table("cycleway_way", { 
-    {
+local cycleway = osm2pgsql.define_way_table("cycleway_way", {{
+    column = 'name',
+    type = 'text'
+}, {
+    column = 'geom',
+    type = 'LineString',
+    not_null = true
+}, {
+    column = 'source',
+    type = 'int8',
+    not_null = true
+}, {
+    column = 'target',
+    type = 'int8',
+    not_null = true
+}, {
+    column = 'kind',
+    type = 'text',
+    not_null = true
+}, {
+    column = 'tags',
+    type = 'jsonb',
+    not_null = true
+}, {
+    column = 'nodes',
+    sql_type = 'int8[] NOT NULL'
+}})
+
+local all_way = osm2pgsql.define_way_table("all_way", {{
+    column = 'name',
+    type = 'text'
+}, {
+    column = 'geom',
+    type = 'LineString',
+    not_null = true
+}, {
+    column = 'source',
+    type = 'int8',
+    not_null = true
+}, {
+    column = 'target',
+    type = 'int8',
+    not_null = true
+}, {
+    column = 'tags',
+    type = 'jsonb',
+    not_null = true
+}, {
+    column = 'nodes',
+    sql_type = 'int8[] NOT NULL'
+}, {
+    column = 'landuse',
+    type = 'text'
+}})
+
+local all_area = osm2pgsql.define_table({
+    name = 'all_area',
+    ids = {
+        type = 'area',
+        id_column = 'way_id'
+    },
+    columns = {{
         column = 'name',
         type = 'text'
     }, {
         column = 'geom',
-        type = 'LineString',
+        type = 'polygon',
         not_null = true
     }, {
-        column = 'source',
-        type = 'int8',
+        column = 'tags',
+        type = 'jsonb',
         not_null = true
     }, {
-        column = 'target',
-        type = 'int8',
-        not_null = true
-    },{
-        column = 'kind',
-        type = 'text',
-        not_null = true
-    }, {
-        column = 'tags', type = 'jsonb',
-        not_null = true
-    }, {
-        column = 'nodes',
-        sql_type = 'int8[] NOT NULL'
-    }
-})
-
-local all_way = osm2pgsql.define_way_table("all_way", { 
-    {
-        column = 'name',
+        column = 'landuse',
         type = 'text'
-    }, {
+    }},
+    indexes = {{
         column = 'geom',
-        type = 'LineString',
-        not_null = true
-    }, {
-        column = 'source',
-        type = 'int8',
-        not_null = true
-    }, {
-        column = 'target',
-        type = 'int8',
-        not_null = true
-    }, {
-        column = 'tags', type = 'jsonb',
-        not_null = true
-    }, {
-        column = 'nodes',
-        sql_type = 'int8[] NOT NULL'
-    }
+        method = 'gist'
+    }}
+
 })
 
-local cycleway_point = osm2pgsql.define_node_table('cycleway_node', {
-    { column = 'name', type = 'text' },
-    { column = 'geom', type = 'Point' },
-    { column = 'tags', type = 'jsonb' },
-    { column = 'adjacents', sql_type = 'int8[]' }
-})
+local cycleway_point = osm2pgsql.define_node_table('cycleway_point', {{
+    column = 'name',
+    type = 'text'
+}, {
+    column = 'geom',
+    type = 'Point'
+}, {
+    column = 'tags',
+    type = 'jsonb'
+}})
 
 function osm2pgsql.process_way(object)
-
-    if object.tags.highway == 'cycleway' or 
-        object.tags.cycleway == "track" or 
-        object.tags["cycleway:left"] == "track" or
-        object.tags["cycleway:right"] == "track" or 
-        object.tags["cycleway:both"] == "track" then
-
+    if object.tags.highway == 'cycleway' or object.tags.cycleway == "track" or object.tags["cycleway:left"] == "track" or
+        object.tags["cycleway:right"] == "track" or object.tags["cycleway:both"] == "track" then
         cycleway:insert({
             name = object.tags.name,
             geom = object:as_linestring(),
@@ -76,59 +104,58 @@ function osm2pgsql.process_way(object)
             tags = object.tags,
             nodes = "{" .. table.concat(object.nodes, ",") .. "}"
         })
-
-    elseif object.tags.bicycle == "designated" or 
-        object.tags["cycleway:left"] == "share_busway" or
-        object.tags["cycleway:right"] == "share_busway" or 
-        object.tags["cycleway:both"] == "share_busway" or
-        object.tags["cycleway:right"] == "lane" or 
-        object.tags["cycleway:left"] == "lane" or
+    elseif object.tags.bicycle == "designated" or object.tags["cycleway:left"] == "share_busway" or
+        object.tags["cycleway:right"] == "share_busway" or object.tags["cycleway:both"] == "share_busway" or
+        object.tags["cycleway:right"] == "lane" or object.tags["cycleway:left"] == "lane" or
         object.tags["cycleway:both"] == "lane" then
-            cycleway:insert({
-                name = object.tags.name,
-                geom = object:as_linestring(),
-                source = object.nodes[1],
-                target = object.nodes[#object.nodes],
-                kind = 'designated',
-                tags = object.tags,
-                nodes = " {" .. table.concat(object.nodes, ",") .. "}"
-            })
-    elseif object.tags.cycleway == "shared_lane" 
-            or object.tags.cycleway == "lane" or 
-            object.tags["cycleway:left"] == "shared_lane" or 
-            object.tags["cycleway:right"] == "shared_lane" or 
-            object.tags["cycleway:both"] == "shared_lane" or 
-            object.tags["bicycle"] == "yes" then
-            cycleway:insert({
-                name = object.tags.name,
-                geom = object:as_linestring(),
-                source = object.nodes[1],
-                target = object.nodes[#object.nodes],
-                kind = 'shared_lane',
-                tags = object.tags,
-                nodes = "{" .. table.concat(object.nodes, ",") .. "}"
-            })
+        cycleway:insert({
+            name = object.tags.name,
+            geom = object:as_linestring(),
+            source = object.nodes[1],
+            target = object.nodes[#object.nodes],
+            kind = 'designated',
+            tags = object.tags,
+            nodes = " {" .. table.concat(object.nodes, ",") .. "}"
+        })
+    elseif object.tags.cycleway == "shared_lane" or object.tags.cycleway == "lane" or object.tags["cycleway:left"] ==
+        "shared_lane" or object.tags["cycleway:right"] == "shared_lane" or object.tags["cycleway:both"] == "shared_lane" or
+        object.tags["bicycle"] == "yes" then
+        cycleway:insert({
+            name = object.tags.name,
+            geom = object:as_linestring(),
+            source = object.nodes[1],
+            target = object.nodes[#object.nodes],
+            kind = 'shared_lane',
+            tags = object.tags,
+            nodes = "{" .. table.concat(object.nodes, ",") .. "}"
+        })
     end
 
-    if object.tags.highway ~= 'motorway' and
-        object.tags.bicycle ~= 'no' and
-        object.tags.highway then
+    if object.tags.highway ~= 'motorway' and object.tags.bicycle ~= 'no' and object.tags.highway then
+        all_way:insert({
+            name = object.tags.name,
+            geom = object:as_linestring(),
+            source = object.nodes[1],
+            target = object.nodes[#object.nodes],
+            tags = object.tags,
+            nodes = "{" .. table.concat(object.nodes, ",") .. "}"
+        })
+    end
 
-            all_way:insert({
-                name = object.tags.name,
-                geom = object:as_linestring(),
-                source = object.nodes[1],
-                target = object.nodes[#object.nodes],
-                tags = object.tags,
-                nodes = "{" .. table.concat(object.nodes, ",") .. "}"
-            })
+    if object.is_closed and object.tags.landuse == 'residential' then
+        all_area:insert({
+            name = object.tags.name,
+            geom = object:as_polygon(),
+            tags = object.tags,
+            landuse = object.tags.landuse
+        })
     end
 end
 
 function osm2pgsql.process_node(object)
-        cycleway_point:insert({
-            name = object.tags.name,
-            geom = object:as_point(),
-            tags = object.tags
-        })
+    cycleway_point:insert({
+        name = object.tags.name,
+        geom = object:as_point(),
+        tags = object.tags
+    })
 end
