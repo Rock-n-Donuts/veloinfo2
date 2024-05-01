e23a199f0c176404621e7e797e25caa0df291c94
Martin Hamel <martin@ma4s.org>
2024-05-01 19:42:45 +0000

    designated .6

--- import.sh
+++ import.sh
@@ -56,8 +56,8 @@ psql -h db -U postgres -d carte -c "
                                                 when tags->>'cycleway:right' = 'shared_lane' then 1 / 0.6
                                                 when tags->>'cycleway' = 'shared_lane' then 1 / 0.6
                                                 when tags->>'highway' = 'residential' then 1 / 0.6
-                                                when tags->>'bicycle' = 'yes' then 1 / 0.5
-                                                when tags->>'bicycle' = 'designated' then 1 / 0.5
+                                                when tags->>'bicycle' = 'yes' then 1 / 0.6
+                                                when tags->>'bicycle' = 'designated' then 1 / 0.6
                                                 when tags->>'highway' = 'tertiary' then 1 / 0.5
                                                 when tags->>'highway' = 'secondary' then 1 / 0.4
                                                 when tags->>'highway' = 'service' then 1 / 0.3
