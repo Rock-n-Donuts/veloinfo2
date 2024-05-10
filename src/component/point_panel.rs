0dca21187a2397af4db1bdf9b598e06df4ec784b
Martin Hamel <martin@ma4s.org>
2024-05-10 15:39:11 +0000

    on affiche près de quoi on clique

--- src/component/point_panel.rs
+++ src/component/point_panel.rs
@@ -1,17 +1,20 @@
-use crate::VeloinfoState;
+use crate::{db::search_db, VeloinfoState};
 use askama::Template;
 use axum::extract::{Path, State};
 
 #[derive(Template)]
 #[template(path = "point_panel.html", escape = "none")]
-pub struct PointPanel {}
+pub struct PointPanel {
+    name: String,
+}
 
 pub async fn point_panel_lng_lat(
     Path((lng, lat)): Path<(f64, f64)>,
     state: State<VeloinfoState>,
 ) -> PointPanel {
-    println!("lng: {}", lng);
-    println!("lat: {}", lat);
-    println!("state: {:?}", state);
-    PointPanel {}
+    let name = match search_db::get_any(&lng, &lat, &state.conn).await.first() {
+        Some(ar) => ar.name.clone(),
+        None => "".to_string(),
+    };
+    PointPanel { name }
 }
--- src/db/search_db.rs
+++ src/db/search_db.rs
@@ -58,6 +58,51 @@ pub async fn get(
         }
 }
 
+pub async fn get_any(lng: &f64, lat: &f64, conn: &sqlx::Pool<Postgres>) -> Vec<SearchResultDB> {
+    match sqlx::query_as(
+            r#"SELECT name, lng, lat FROM (
+                    SELECT 
+                        street || ', ' || city as name, 
+                        CASE
+                            WHEN ST_GeometryType(geom) = 'ST_Point' THEN ST_X(ST_Transform(geom, 4326))
+                            ELSE ST_X(ST_Transform(ST_PointN(geom, 1), 4326))
+                        END as lng,
+                        CASE
+                            WHEN ST_GeometryType(geom) = 'ST_Point' THEN ST_Y(ST_Transform(geom, 4326))
+                            ELSE ST_Y(ST_Transform(ST_PointN(geom, 1), 4326))
+                        END as lat,
+                        geom,
+                        ROW_NUMBER() OVER(PARTITION BY city, street ORDER BY ar.geom<-> ST_Transform(ST_SetSRID(ST_MakePoint($1, $2), 4326), 3857)) AS rn
+                    FROM address_range ar 
+                    where ST_DWithin(geom, ST_Transform(ST_SetSRID(ST_MakePoint($1, $2), 4326), 3857), 1000)
+                    union
+                    select 
+                        distinct on (name || ' ' || coalesce(tags::JSONB->>'addr:street', '') || ' ' || coalesce(tags::JSONB->>'addr:city', ''))
+                        name || ' ' || coalesce(tags::JSONB->>'addr:street', '') || ' ' || coalesce(tags::JSONB->>'addr:city', '') as name,
+                        ST_X(ST_Transform(geom, 4326)) as lng,
+                        ST_Y(ST_Transform(geom, 4326)) as lat,
+                        geom,
+                        1 as rn
+                    from name_query
+                    where ST_DWithin(geom, ST_Transform(ST_SetSRID(ST_MakePoint($1, $2), 4326), 3857), 1000)
+            ) t WHERE name is not null
+            order by geom <-> ST_Transform(ST_SetSRID(ST_MakePoint($1, $2), 4326), 3857)
+            limit 20;
+           "#,
+        )
+        .bind(lng)
+        .bind(lat)
+        .fetch_all(conn)
+        .await
+        {
+            Ok(ar) => ar,
+            Err(e) => {
+                eprintln!("Error getting SearchResultDB: {}", e);
+                Vec::new()
+            }
+        }
+}
+
 pub async fn get_with_adress(
     number: &i64,
     request: &String,
